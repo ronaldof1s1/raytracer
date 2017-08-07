250eda4a61cdb245dc1d93c7c534092003b9ae24
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-08-07 11:17:59 -0300

    generating output file name

--- src/render.cpp
+++ src/render.cpp
@@ -24,12 +24,34 @@ std::vector<std::string> split_string(std::string str, std::string delimiter) {
   return *splitted;
 }
 
+std::string parse_file_name(std::string line) {
+  del_comments(line);
+  std::vector<std::string> strs = split_string(line, " ");
+  std::string file_name = "";
+  if (strs[0].compare("NAME") + strs[1].compare("=") == 0) {
+    for (size_t i = 2; i < strs.size(); i++) {
+      file_name += strs[i];
+      if (i != strs.size() -1) {
+        file_name += " ";
+      }
+    }
+  }
+  std::cout << file_name << '\n';
+  return file_name;
+}
+
 
 int main(int argc, char const *argv[]) {
   std::string input_file_name = argv[0];
   // std::ifstream input_file(input_file_name, std::ios::in);
   std::ifstream input_file("input_file.txt", std::ios::in);
+  std::string output_file_name;
+  
   if (input_file.is_open()) {
+    std::string line;
+    //getting file name
+    std::getline(input_file, line);
+    output_file_name = parse_file_name(line);
 
     input_file.close();
   }
