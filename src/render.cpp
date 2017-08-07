9764a6ccb095d5ca3ff896213bbd7356cb08eb1b
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-08-07 11:15:59 -0300

    Create comment_deletions and split_String functions

--- src/render.cpp
+++ src/render.cpp
@@ -3,6 +3,28 @@
 #include <string>
 #include <vector>
 
+void del_comments(std::string& str){
+  size_t pos = str.find("#");
+  std::string aux = str.substr(0,pos);
+  str = aux;
+}
+
+std::vector<std::string> split_string(std::string str, std::string delimiter) {
+  std::vector<std::string> *splitted = new std::vector<std::string>();
+  int i = 0;
+  size_t pos = str.find(delimiter);
+  while (pos != std::string::npos) {
+    splitted->push_back(str.substr(i,pos-i));
+    i = ++pos;
+    pos = str.find(delimiter, pos);
+    if (pos == std::string::npos) {
+      splitted->push_back(str.substr(i,str.length()));
+    }
+  }
+  return *splitted;
+}
+
+
 int main(int argc, char const *argv[]) {
   std::string input_file_name = argv[0];
   // std::ifstream input_file(input_file_name, std::ios::in);
