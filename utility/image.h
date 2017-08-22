57802a05dd62dbf9dbf046ec05b3fd0b262d3823
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-08-21 23:49:44 -0300

    finish scene class and create image class

---
+++ utility/image.h
@@ -0,0 +1,11 @@
+#ifndef IMAGE_H_
+#define IMAGE_H_
+	
+class image{
+	int width, height;
+	char * pixels;
+public:
+	
+}
+
+#endif
--- utility/scene.h
+++ utility/scene.h
@@ -8,6 +8,19 @@ class scene{
 	std::list< hitable > objects;
 
 	// TODO: constructors & gettes & methods
-};
+	scene(std::list< hitable > obj){
+		objects = obj;
+	}
+
+	std::list< hitable > get_all_objects() {return objects}
+	hitable get_object(int i) {
+		std::list< hitable >::iterator it = objects.begin();
+		while(i-- > 0 && it !=objects.end()){
+			it.next();
+		}
+		return *it;	
+	}
+	void add_object(hitable object) {objects.push_back(object)}
+ };
 
 #endif
