60c9c9bb02a588ebefa9de8594b1064eca73131f
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-09-07 11:39:30 -0300

    changed shininess to specular_exponent

--- libs/Material.h
+++ libs/Material.h
@@ -6,27 +6,27 @@
     RGB k_d; // The coefficient of diffuse reflectance
     RGB k_s; // The coefficient of specular reflectance
 
-    int shininess; // Shininess coefficient for reflecting light on some shaders
+    int specular_exponent; // Specular exponent for reflecting light on some shaders
 
     //Constructors
     Material(int sh=0){
       k_a = RGB(0.1f);
       k_d = RGB(0.d); //Black sphere
       k_s = RGB(0.d);
-      shininess = sh;
+      specular_exponent = sh;
     }
     Material(RGB rgb_, int sh=0){
       k_a = RGB(0.1f);
       k_d = rgb_;
       k_s = RGB(0.d);
-      shininess = sh;
+      specular_exponent = sh;
     }
 
     Material(RGB k_a_, RGB k_d_, RGB k_s_, int sh=0 ){
       k_a = k_a_;
       k_d = k_d_;
       k_s = k_s_;
-      shininess = sh;
+      specular_exponent = sh;
     }
   };
 #endif
