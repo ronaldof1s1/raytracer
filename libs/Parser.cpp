e326c51ae1ccf2deec549ad6c93356bc191f8034
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-09-10 16:35:17 -0300

    parsing type, codification, width, height, max_color and antialiasing

--- libs/Parser.cpp
+++ libs/Parser.cpp
@@ -20,21 +20,77 @@ void split_string(std::string &str, std::string &delim, std::vector< std::string
   }
 }
 
-bool parse_type(std::vector< std::strig > &words, int &type){}
-bool parse_width(){}
-bool parse_height(){}
-bool parse_max_color(){}
+bool parse_type(std::vector< std::strig > &words, int &type){
+  if(words[1] == "="){
+    if(words[2] == "ppm"){
+      type = 3;
+      return true;
+    }
+  }
+  return false;
+}
+
+bool parse_codification(std::vector< std::strig > &words, int &type){
+  if(words[1] == "="){
+    if(words[2] == "ascii"){
+      return true;
+    }
+    else if(words[2] == "binary"){
+      type += 3;
+      return true;
+    }
+  }
+  return false;
+}
+
+bool parse_width(std::vector< std::strig > &words, int &width){
+  if(words[1] == "="){
+    width = std::stoi(words[2]);
+    return true;
+  }
+  return false;
+}
+
+bool parse_height(std::vector< std::strig > &words, int &height){
+  if(words[1] == "="){
+    height = std::stoi(words[2]);
+    return true;
+  }
+  return false;
+}
+
+bool parse_max_color(std::vector< std::strig > &words, int &max_color){
+  if(words[1] == "="){
+    max_color = std::stoi(words[2]);
+    return true;
+  }
+  return false;
+}
+
+bool parse_antialiasing(std::vector< std::strig > &words, int &antialiasing){
+  if(words[1] == "="){
+    antialiasing = std::stoi(words[2]);
+    return true;
+  }
+  return false;
+}
+
+bool parse_object(Image &image, int &line_number){}
 bool parse_background(Image &image, int &line_number){}
 bool parse_scene(Image &image, int &line_number){}
-bool parse_object(Image &image, int &line_number){}
-bool parse_sphere(){}
-bool parse_file_name(std::vector< std::strig > &words){}
-bool parse_image(Image &image, int &line_number){
+void parse_file_name(Image &image, std::string output_file_name){}
+
+bool parse_image(Image &image, Shader *shader, int &line_number){
 
   int type, max_color, width, height, antialiasing;
+  type = max_color = width = height = 0;
+  antialiasing = 1;
+
+  bool has_type, has_max_color, has_width, has_height, has_codification;
+  has_type = has_max_color = has_width = has_height = has_codification = false;
+
   Scene scene;
   Camera cam;
-
   while (std::getline(input_stream, line)) {
 
     line_number++;
@@ -52,30 +108,35 @@ bool parse_image(Image &image, int &line_number){
           if(!parse_type(words, type)){
             return false;
           }
+          has_type = true;
           break;
 
         case "codification":
           if(!parse_codification(words, type)){
             return false;
           }
+          has_codification = true;
           break;
 
         case "width":
           if(!parse_width(words, width)){
             return false;
           }
+          has_width = true;
           break;
 
         case "height":
           if (!parse_height(words, height)) {
             return false;
           }
+          has_height = true;
           break;
 
         case "maxcolor":
           if(!parse_max_color(words, max_color)){
             return false;
           }
+          has_max_color = true;
           break;
 
         case "antialiasing":
@@ -108,7 +169,17 @@ bool parse_image(Image &image, int &line_number){
 
           case "END":
             //final line must be "END IMAGE"
-            return (words[1] == "IMAGE") ? true : false;
+            if(has_type && has_width && has_height && has_max_color){
+              image = Image(type, max_color, width, height, scene, camera, antialiasing);
+              std::string name;
+              if(!parse_file_name(image, name)){
+                return false;
+              }
+              return (words[1] == "IMAGE") ? true : false;
+            }
+            else{
+              return false;
+            }
             break;
 
 
