608ba30461e7b47aca666ffb9df84eaae362c9af
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-09-10 15:50:01 -0300

    add antialiasing to Image.h

--- libs/Image.h
+++ libs/Image.h
@@ -6,31 +6,36 @@
 
 
 class Image{
+
+public:
+
 	int type; //ppm ascii, binary, etc
 	int max_color;
 	int width, height;
+	int antialiasing;
 	Scene scene; //background, list of objects, lights and ambient light;
 	Camera camera;
 
-public:
 	//Constructors
 
 	Image(){}
-	Image(int t, int w, int h, Scene &sce, Camera &cam){
+	Image(int t, int w, int h, Scene &sce, Camera &cam, int a = 1){
 		width = w;
 		height = h;
 		scene = sce;
 		camera = cam;
 		type = t;
+		antialiasing = a;
 	}
 
-	Image(int t, int color, int w, int h, Scene &sce, Camera &cam){
+	Image(int t, int color, int w, int h, Scene &sce, Camera &cam, int a = 1){
 		width = w;
 		height = h;
 		scene = sce;
 		camera = cam;
 		type = t;
 		max_color = color;
+		antialiasing = a;
 	}
 
 	//Getters
@@ -38,10 +43,12 @@ public:
 	int get_height(){return height;}
 	int get_type(){return type;}
 	int get_max_color(){return max_color;}
+	int get_antialiasing(){return antialiasing;}
 
 	Camera get_camera(){return camera;}
 	Scene get_scene(){return scene;}
 	Background get_background(){return scene.get_background();}
+
 };
 
 #endif
