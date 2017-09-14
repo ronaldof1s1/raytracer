a73714fb88e3acad619a1d456d4b9175460911bc
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-09-13 22:25:30 -0300

    implemented scatter function of lambertian

--- libs/materials/Lambertian.h
+++ libs/materials/Lambertian.h
@@ -9,6 +9,14 @@ public:
   Lambertian():Material(){}
   Lambertian(RGB rgb_ ):Material(rgb_){}
   Lambertian(RGB k_a, RGB k_d):Material(k_a, k_d, RGB(0.0)){}
+  bool scatter(const Ray &ray_in, const hit_record &rec, Ray &scattered) const override;
 };
+
+bool Lambertian::scatter(const Ray &ray_in, const hit_record &rec, Ray &scattered) const {
+  Vector3 target = rec.p + rec.normal + random_vector_in_unit_sphere();
+  Point3 origin = rec.p + 0.01 * rec.normal;
+  scattered = Ray(origin, target - origin);
+  return true;
+}
 
 #endif
--- libs/materials/Shiny.h
+++ libs/materials/Shiny.h
@@ -8,6 +8,9 @@ public:
   //Constructors
   Shiny():Material(){}
   Shiny(RGB k_a, RGB k_d, RGB k_s, double specular_exponent = 4):Material(k_a, k_d, k_s, specular_exponent){}
+  bool scatter(const Ray &ray_in,  hit_record &rec, Ray &scattered) const override;
 };
+bool Shiny::scatter(const Ray &ray_in, const hit_record &rec, Ray &scattered) const {
 
+}
 #endif
