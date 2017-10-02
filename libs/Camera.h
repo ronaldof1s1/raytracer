c6267f3f5c047eca7ab548109522e66f4a7a154a
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-10-02 18:06:51 -0300

    back to old camera mode

--- libs/Camera.h
+++ libs/Camera.h
@@ -1,11 +1,11 @@
-#ifndef CAMERA_H_
-#define CAMERA_H_
+#ifndef _CAMERA_H_
+#define _CAMERA_H_
 
 #include "utility/Vector3.h"
 #include "utility/Ray.h"
 #include "View_Plane.h"
 #include <cmath>
-# define M_PI           3.14159265358979323846
+#define M_PI           3.14159265358979323846
 
 class Camera{
 public:
@@ -33,13 +33,38 @@ public:
 	std::tuple<Vector3, Vector3, Vector3> get_frame() {return std::make_tuple(u,v,w);};
 
 	//Getting ray shoot from the camera
-	virtual Ray get_ray(double u, double v);
+	// virtual Ray get_ray(double u, double v); 		TEMPORARY
+	inline Ray get_ray(double u, double v);
 
-	Ray get_ray(double row, double col, int n_rows, int n_cols);
+	inline Ray get_ray(double row, double col, int n_rows, int n_cols);
 
 
-	void set_frame(Point3 look_from, Point3 look_at, Vector3 up);
+	inline void set_frame(Point3 look_from, Point3 look_at, Vector3 up);
 
 };
+
+inline Ray Camera::get_ray(double row, double col, int n_rows, int n_cols){
+  double u = double(col) / double( n_cols ); // walked u% of the horizontal dimension of the view plane.
+  double v = double(row) / double( n_rows ); // walked v% of the vertical dimension of the view plane.
+
+  return get_ray(u, v);
+}
+
+inline void Camera::set_frame(Point3 look_from, Point3 look_at, Vector3 up){
+  // vector3 u, v, w;
+  Vector3 gaze = look_from - look_at;
+
+  w = unit_vector(gaze);
+  u = unit_vector(cross(up, w));
+  v = unit_vector(cross(w,u));
+
+  origin = look_from;
+}
+
+inline Ray Camera::get_ray(double u, double v){
+  Vector3 target = view_plane.lower_left_corner + u* view_plane.horizontal_axis + v*view_plane.vertical_axis;
+  return Ray(origin, target);
+}
+
 
 #endif
