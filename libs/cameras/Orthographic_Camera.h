335f4c81aeb4904a9c2de9a4fbd7e20d7dc57e21
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-10-02 19:06:35 -0300

    add first version of oblique and perspective camera

--- libs/cameras/Orthographic_Camera.h
+++ libs/cameras/Orthographic_Camera.h
@@ -5,17 +5,25 @@
 
 class Orthographic_Camera : public Camera{
 public:
+  Orthographic_Camera(Point3 left, Point3 right, Point3 bottom, Point3 top, Vector3 vp_normal = Vector3(0)):Camera(){
 
-  // Orthographic_Camera(double left, double right, double bottom, double top, Vector3 vp_normal = Vector3(0)):Camera(){
-  //
-  // }
+    if (vp_normal.X() == 0 && vp_normal.Y() == 0 && vp_normal.Z() == 0){
+      vp_normal = -std::get<2>(frame);
+    }
+
+    direction = unit_vector(vp_normal);
+    Vector3 llc = left + bottom;
+    Vector3 ha = right - left;
+    Vector3 va = top - bottom;
+    view_plane = View_Plane(llc, ha, va, vp_normal);
+  }
 
   virtual Ray get_ray(double u, double v);
 };
 
 Ray Orthographic_Camera::get_ray(double u, double v){
-  Point3 ray_origin = origin + u*std::get<0>(frame) + v*std::get<1>(frame);
-  Vector3 ray_target = unit_vector(-std::get<2>);
+  Point3 ray_origin = view_plane.lower_left_corner + u*view_plane.horizontal_axis + v*view_plane.vertical_axis;
+  Vector3 ray_target = view_plane.normal;
   return Ray(ray_origin, ray_target);
 }
 
--- libs/cameras/Perspective_Camera.h
+++ libs/cameras/Perspective_Camera.h
@@ -15,16 +15,24 @@ public:
     double half_width = aspect * half_height;
 
     if (vp_normal.X() == 0 && vp_normal.Y() == 0 && vp_normal.Z() == 0){
-      vp_normal = std::get<2>(frame);
+      vp_normal = -std::get<2>(frame);
     }
 
+    vp_v = cross(std::get<0>(frame), vp_normal);
+    vp_u = cross(vp_normal, vp_u);
+
+    llc = origin + dist_to_focus - half_width*vp_u - half_height*vp_v - vp_normal;
+    ha = 2*half_width*vp_u;
+    va = 2*half_height*vp_v;
+    view_plane = View_Plane(llc,ha,va, vp_normal);
   }
 
   virtual Ray get_ray(double u, double v);
 };
 
 Ray Perspective_Camera::get_ray(double u, double v){
-  return Ray(Point3(0), Vector3(0));
+  Vector3 target = view_plane.lower_left_corner + u*view_plane.horizontal_axis + v*view_plane.vertical_axis;
+  return Ray(origin, unit_vector(target));
 }
 
 #endif
