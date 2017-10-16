3c8cb7b416002ef9df285a5f4d5cc34cc9a084df
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-10-16 15:11:18 -0200

    create 3 types of light: pointlight, spotlight and directional light

---
+++ libs/lights/Pointlight.h
@@ -0,0 +1,24 @@
+#ifndef POINTLIGHT_H_
+#define POINTLIGHT_H_
+
+#include "../Light.h"
+
+class Pointlight : public Light {
+
+  public:
+    Point3 source;
+
+    //Constructors
+    Pointlight():Light(){ source = Point3(0.0,0.0,0.0); }
+    Pointlight(RGB intensity):Light(intensity){ source = Point3(0.0,0.0,0.0); }
+    Pointlight(Point3 src, RGB intensity):Light(intensity){ source = src; }
+
+    Point3 get_source(){ return source; }
+
+    std::pair<Vector3, RGB> Illuminate(Point3 point) const override {
+        Vector3 light_direction = unit_vector(source - point);
+        return std::make_pair(light_direction, intensity);
+    }
+  };
+
+#endif
---
+++ libs/lights/Spotlight.h
@@ -0,0 +1,13 @@
+#ifndef _SPOTLIGHT_H_
+#define _SPOTLIGHT_H_
+
+class Spotlight : public Light{
+public:
+
+std::pair<Vector3, RGB> Illuminate(Point3 p) const override{
+  
+}
+
+};
+
+#endif
