384f7df68229249b14bb062c2ba7cfe62df75102
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-10-16 15:11:53 -0200

    camera working

--- libs/Camera.h
+++ libs/Camera.h
@@ -3,36 +3,58 @@
 
 #include "utility/Vector3.h"
 #include "utility/Ray.h"
-#include "View_Plane.h"
-#include <cmath>
-# define M_PI           3.14159265358979323846
 
 class Camera{
 public:
 	// fields
-	Point3 origin;
-	View_Plane view_plane;
-	std::tuple<Vector3, Vector3, Vector3> frame; //x,y,z ortogonal
+	Vector3 origin;
+	Vector3 lower_left_corner;
+	Vector3 vertical_axis;
+	Vector3 horizontal_axis;
 
 	// Constructors & getters
 	Camera(){
-
+		origin = Point3(0);
+		lower_left_corner = Point3(0);
+		vertical_axis = Vector3(0);
+		horizontal_axis = Vector3(0);
+	};
+	Camera(Point3 origin_, Point3 llc, Vector3 va, Vector3 ha){
+		origin = origin_;
+		lower_left_corner = llc;
+		vertical_axis = va;
+		horizontal_axis = ha;
 	};
 
+
 	//Getters
-	Vector3 get_lower_left_corner() {return view_plane.lower_left_corner;};
-	Vector3 get_vertical_axis() {return view_plane.vertical_axis;};
-	Vector3 get_horizontal_axis() {return view_plane.horizontal_axis;};
+	Vector3 get_lower_left_corner() {return lower_left_corner;};
+	Vector3 get_vertical_axis() {return vertical_axis;};
+	Vector3 get_horizontal_axis() {return horizontal_axis;};
 	Vector3 get_origin() {return origin;};
-	std::tuple<Vector3, Vector3, Vector3> get_frame() {return frame;};
 
 	//Getting ray shoot from the camera
-	virtual Ray get_ray(double u, double v);
+	Ray get_ray(double u, double v){
+		// Determine the ray's direction, based on the pixel coordinate (col,row).
+		// We are mapping (matching) the view plane (vp) to the image.
+		// To create a ray we need: (a) an origin, and (b) an end point.
+		//
+		// (a) The ray's origin is the origin of the camera frame (which is the same as the world's frame).
+		//
+		// (b) To get the end point of ray we just have to 'walk' from the
+		// vp's origin + horizontal displacement (proportional to 'col') +
+		// vertical displacement (proportional to 'row').
+		Point3 end_point = lower_left_corner + u*horizontal_axis + v*vertical_axis;
+		Ray r (origin, unit_vector(end_point - origin));
+		return r;
+	}
 
-	Ray get_ray(double row, double col, int n_rows, int n_cols);
+	Ray get_ray(double row, double col, int n_rows, int n_cols){
+		double u = double(col) / double( n_cols ); // walked u% of the horizontal dimension of the view plane.
+		double v = double(row) / double( n_rows ); // walked v% of the vertical dimension of the view plane.
 
-
-	void set_frame(Point3 look_from, Point3 look_at, Vector3 up);
+		return get_ray(u, v);
+	}
 
 };
 
